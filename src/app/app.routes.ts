6365170b394575a699708d7655595b7b4b1d401f
Benjamin <benjaschell@gmail.com>
2024-05-07 02:30:34 -0300

    basicRuteo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componets/home/home.component';
+import { QuienSoyComponent } from './componets/quien-soy/quien-soy.component';
+import { ErrorComponent } from './componets/error/error.component';
+import { LoginComponent } from './componets/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: "full" },
+    { path: 'home', component: HomeComponent },
+    { path: 'quiensoy', component: QuienSoyComponent },
+    { path: 'login', component: LoginComponent },
+    // {
+    //     path: 'products', component: ProductsComponent,
+    //     // Al declarar un children es clave que le pongamos a este el <router-outlet /> en el html
+    //     children:
+    //         [
+    //             {
+    //                 path: ":id",
+    //                 component: ProductDetailComponent
+    //             }
+    //         ]
+    // },
+    { path: '**', component: ErrorComponent },
+];
