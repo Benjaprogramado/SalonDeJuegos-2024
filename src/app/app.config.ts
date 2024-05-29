81c61054c16a7ec21fe21a41b2d5d8ab36f7a5cc
Benjamin <benjaschell@gmail.com>
2024-05-29 02:26:40 -0300

    instalandoFireDatabase2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"saladejuegos-2024","appId":"1:321908198090:web:be5efce67b7c7229100233","storageBucket":"saladejuegos-2024.appspot.com","apiKey":"AIzaSyAdls8Cx4CEE4x2Feia9EYoFyIRIHY383U","authDomain":"saladejuegos-2024.firebaseapp.com","messagingSenderId":"321908198090"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
