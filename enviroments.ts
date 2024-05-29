11d9cc6a9a202fec6aef3a13a88ba44ac1f1964a
Benjamin <benjaschell@gmail.com>
2024-05-29 02:35:08 -0300

    instalandoFireDatabase3

---
+++ enviroments.ts
@@ -0,0 +1,10 @@
+export const environment =
+{
+    "projectId": "saladejuegos-2024",
+    "appId": "1:321908198090:web:be5efce67b7c7229100233",
+    "storageBucket": "saladejuegos-2024.appspot.com",
+    "apiKey": "AIzaSyAdls8Cx4CEE4x2Feia9EYoFyIRIHY383U",
+    "authDomain": "saladejuegos-2024.firebaseapp.com",
+    "messagingSenderId": "321908198090"
+}
+    ;
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,8 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../../enviroments';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"saladejuegos-2024","appId":"1:321908198090:web:be5efce67b7c7229100233","storageBucket":"saladejuegos-2024.appspot.com","apiKey":"AIzaSyAdls8Cx4CEE4x2Feia9EYoFyIRIHY383U","authDomain":"saladejuegos-2024.firebaseapp.com","messagingSenderId":"321908198090"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp(environment)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
